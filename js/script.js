f84433d0bc45426a4f29c7a12a2d3f4e2a87b2d7
Marcos Quirino Garcia <marcosqgarcia@gmail.com>
2022-12-03 22:42:04 -0300

    javascript adicionado

--- js/script.js
+++ js/script.js
@@ -1,44 +1,18 @@
 let numeros = [0, 0, 0, 0, 0, 0]
 
-function limpa(){
-
-    if(numeros[0] != 0){
-
-        window.document.getElementsByTagName('div')[numeros[0]].style.backgroundColor = 'white' 
-        window.document.getElementsByTagName('div')[numeros[1]].style.backgroundColor = 'white' 
-        window.document.getElementsByTagName('div')[numeros[2]].style.backgroundColor = 'white' 
-        window.document.getElementsByTagName('div')[numeros[3]].style.backgroundColor = 'white' 
-        window.document.getElementsByTagName('div')[numeros[4]].style.backgroundColor = 'white' 
-        window.document.getElementsByTagName('div')[numeros[5]].style.backgroundColor = 'white' 
-    }
-
-    
-}
 
 function geraNumeros(){
 
     var opcaoAleatorio = window.document.getElementById('idAleatorio')
-    var opcaoAleatorioParImpar = window.document.getElementById('idAleParImpar')
     var opcaoLinha = window.document.getElementById('idLinhas')
-    var opcaoLinhaParImpar = window.document.getElementById('idLinhaParImpar')
 
     limpa()
     
     if(opcaoAleatorio.checked == true){
 
-        console.log('modo aleatorio')
+        //console.log('modo aleatorio')
 
         aleatorios()
-
-        naTela()
-
-    }
-    
-    if(opcaoAleatorioParImpar.checked == true){
-
-        console.log('modo aleatorio par impar')
-
-        aleatoriosParImpar()
 
         naTela()
 
@@ -46,74 +20,28 @@ function geraNumeros(){
 
     if(opcaoLinha.checked == true){
 
-        console.log('modo por linha')
+        //console.log('modo por linha')
 
         aleatoriosPorLinha()
 
         naTela()
     }
 
-    if(opcaoLinhaParImpar.checked == true){
+}
 
-        console.log('modo por linha par impar')
+function limpa(){
 
-        aleatoriosPorLinhaParImpar()
+    if(numeros[0] != 0){
 
-        naTela()
+        window.document.getElementsByTagName('div')[numeros[0]-1].style.backgroundColor = 'white' 
+        window.document.getElementsByTagName('div')[numeros[1]-1].style.backgroundColor = 'white' 
+        window.document.getElementsByTagName('div')[numeros[2]-1].style.backgroundColor = 'white' 
+        window.document.getElementsByTagName('div')[numeros[3]-1].style.backgroundColor = 'white' 
+        window.document.getElementsByTagName('div')[numeros[4]-1].style.backgroundColor = 'white' 
+        window.document.getElementsByTagName('div')[numeros[5]-1].style.backgroundColor = 'white' 
     }
 
-}
-
-function naTela(){
     
-    window.document.getElementsByTagName('div')[numeros[0]].style.backgroundColor = 'red' 
-    window.document.getElementsByTagName('div')[numeros[1]].style.backgroundColor = 'red' 
-    window.document.getElementsByTagName('div')[numeros[2]].style.backgroundColor = 'red' 
-    window.document.getElementsByTagName('div')[numeros[3]].style.backgroundColor = 'red' 
-    window.document.getElementsByTagName('div')[numeros[4]].style.backgroundColor = 'red' 
-    window.document.getElementsByTagName('div')[numeros[5]].style.backgroundColor = 'red'
-}
-
-function aleatoriosPorLinha(){
-    
-    numeros [0] = Math.floor(Math.random() * (10 - 1 + 1) +1) 
-    numeros [1] = Math.floor(Math.random() * (20 - 11 + 1)) + 11
-    numeros [2] = Math.floor(Math.random() * (30 - 21 + 1)) + 21
-    numeros [3] = Math.floor(Math.random() * (40 - 31 + 1)) + 31
-    numeros [4] = Math.floor(Math.random() * (50 - 41 + 1)) + 41
-    numeros [5] = Math.floor(Math.random() * (60 - 50)) + 51  
-
-    console.log(numeros)
-
-}
-
-function aleatoriosPorLinhaParImpar(){
-
-    do{    
-        numeros [0] = Math.floor(Math.random() * (10 - 1 + 1) +1) 
-        numeros [1] = Math.floor(Math.random() * (20 - 11 + 1)) + 11
-        numeros [2] = Math.floor(Math.random() * (30 - 21 + 1)) + 21
-        numeros [3] = Math.floor(Math.random() * (40 - 31 + 1)) + 31
-        numeros [4] = Math.floor(Math.random() * (50 - 41 + 1)) + 41
-        numeros [5] = Math.floor(Math.random() * (60 - 50)) + 51  
-
-        console.log(numeros)
-
-    }while(comparaNumeros() == false)
-
-    function comparaNumeros(){
-
-        if(numeros[0] % 2 === 0 && numeros[1] % 2 === 1 && numeros[2] % 2 === 0 && numeros[3] % 2 === 1 && numeros[4] % 2 === 0 && numeros[5] % 2 === 1){
-            
-            return true
-
-        }else{
-            
-            return false
-        }
-
-    }
-
 }
 
 function aleatorios(){
@@ -172,68 +100,26 @@ function aleatorios(){
     
 }
 
-
-function aleatoriosParImpar(){
-
-    do{
-
-        numerosAleatorios()
-        console.log(numeros)
-
-    }while(comparaNumeros() == false)
-
-
-
-
-    function numerosAleatorios(){
+function naTela(){
     
-        for (let index = 0; index < numeros.length; index++) {
-            
-            numeros [index] = Math.floor(Math.random() * (60 - 1 + 1) + 1) 
-            
-        }
-
-        
-    }
-
-
-
-    function comparaNumeros(){
-        
-            
-        if(numeros[0] % 2 === 0 && numeros[1] % 2 === 1 && numeros[2] % 2 === 0 && numeros[3] % 2 === 1 && numeros[4] % 2 === 0 && numeros[5] % 2 === 1){
-            
-            if(numeros[0] == numeros[1] || numeros[0] == numeros[2] || numeros[0] == numeros[3] || numeros[0] == numeros[4] || numeros[0] == numeros[5]){
-
-                return false
-    
-            }else if (numeros[1] == numeros[2] || numeros[1] == numeros[3] || numeros[1] == numeros[4] || numeros[1] == numeros[5]){
-                
-                return false
-    
-            }else if (numeros[2] == numeros[3] || numeros[2] == numeros[4] || numeros[2] == numeros[5]){
-                
-                return false
-    
-            }else if (numeros[3] == numeros[4] || numeros[3] == numeros[5]){
-                
-                return false
-    
-            }else if (numeros[4] == numeros[5]){
-                
-                return false
-    
-            }else{
-    
-                return true
-            }
-
-        }else{
-            
-            return false
-        }
-         
-        
-    }
-    
+    window.document.getElementsByTagName('div')[numeros[0]-1].style.backgroundColor = 'red' 
+    window.document.getElementsByTagName('div')[numeros[1]-1].style.backgroundColor = 'red' 
+    window.document.getElementsByTagName('div')[numeros[2]-1].style.backgroundColor = 'red' 
+    window.document.getElementsByTagName('div')[numeros[3]-1].style.backgroundColor = 'red' 
+    window.document.getElementsByTagName('div')[numeros[4]-1].style.backgroundColor = 'red' 
+    window.document.getElementsByTagName('div')[numeros[5]-1].style.backgroundColor = 'red'
 }
+
+function aleatoriosPorLinha(){
+    
+    numeros [0] = Math.floor(Math.random() * (10 - 1 + 1) +1) 
+    numeros [1] = Math.floor(Math.random() * (20 - 11 + 1)) + 11
+    numeros [2] = Math.floor(Math.random() * (30 - 21 + 1)) + 21
+    numeros [3] = Math.floor(Math.random() * (40 - 31 + 1)) + 31
+    numeros [4] = Math.floor(Math.random() * (50 - 41 + 1)) + 41
+    numeros [5] = Math.floor(Math.random() * (60 - 50)) + 51  
+
+    //console.log(numeros)
+
+}
+
